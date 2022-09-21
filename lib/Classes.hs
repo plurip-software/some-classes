cbe9112905247acbce28e9a90b6626e8042a9071
plurip <sinandavid@hotmail.de>
2022-09-21 12:46:20 +0200

    removed Parser, and comments

--- lib/Classes.hs
+++ lib/Classes.hs
@@ -1,4 +1,6 @@
-module Main (Parser, StringParser, Property) where
+{-# LANGUAGE MultiParamTypeClasses #-}
+
+module Classes (Parser, Property) where
 
 -- | ## Properties
 -- 
@@ -8,13 +10,8 @@ class (Show a, Show b) => Parser a b where
     serialize :: a -> b
     parse     :: b -> Maybe a
 
-class Parser a String => Parser (StringParser a) where
-    stringify :: a -> String
-    parse     :: String -> Maybe a
-
 class Property a b where
     success :: a -> b -> Either a b
     failure :: a -> b -> Either a b
-    first   :: a -> b -> a            --^ The choice between two options is the main property of the boolean type 
-                                      --^ the first x y can be interpreted as True... 
-    second  :: a -> b -> b            --^ ...and the second as False 
+    first   :: a -> b -> a
+    second  :: a -> b -> b
